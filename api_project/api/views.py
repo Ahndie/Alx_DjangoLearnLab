1811a5fe9225d0ef58f8462bc7bc1ca3f54aa7c7
Andrew Ofosu Bamfo <andrewbamfo@gmail.com>
2024-11-24 17:22:49 +0100

    error fixed

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,9 @@
-from rest_framework.generics import ListAPIView
-from .models import Book
-from .serializers import BookSerializer
+from rest_framework.generics import ListAPIView  # Import generics.ListAPIView
+from .models import Book  # Import the Book model
+from .serializers import BookSerializer  # Import the BookSerializer
 
 class BookList(ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    queryset = Book.objects.all()  # Define the queryset for all Book objects
+    serializer_class = BookSerializer  # Define the serializer for the data
+
 
